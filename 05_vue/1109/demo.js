02c9b8a2382ddd90e4790a74e0e0bbd1f3e1c941
jq <1056967990@qq.com>
2017-11-09 20:02:08 +0800

    日常学习

---
+++ 05_vue/1109/demo.js
@@ -0,0 +1,9 @@
+function Vue(aa) {
+    var obj = aa.data();
+
+    var oInputs = document.querySelectorAll('[v-model]');
+    for (var i = 0; i < oInputs.length; i++) {
+        var oInput = oInputs[i];
+
+    }
+}
