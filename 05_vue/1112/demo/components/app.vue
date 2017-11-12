cf904f7e9fa77b883f45c2606367d24ef3e40bcb
jq <1056967990@qq.com>
2017-11-12 15:44:54 +0800

    配置文件

--- 05_vue/1112/demo/components/app.vue
+++ 05_vue/1112/demo/components/app.vue
@@ -2,6 +2,7 @@
   <!-- 要有一个根标签  有且只有一个-->
   <div>
       <h3>hello,components呆橘</h3>
+      <img src="../images/back1.jpg" alt="">
   </div>
 </template>
 
--- 05_vue/1112/demo/js/main.js
+++ 05_vue/1112/demo/js/main.js
@@ -1,5 +1,5 @@
 
-// import Vue from 'vue';
+import Vue from 'vue';
 import Jq from '../components/app.vue';
 
 new Vue({
